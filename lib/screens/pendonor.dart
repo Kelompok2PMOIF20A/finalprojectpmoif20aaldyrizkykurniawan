e28011c18371558405afcb673ac149845861a5e6
ArduDarugutni <theviel77@gmail.com>
2022-07-05 04:22:24 +0700

    Membuat pendonor.dart/halaman menampilkan data pendonor

---
+++ lib/screens/pendonor.dart
@@ -0,0 +1,39 @@
+import 'package:finalprojectpmoif20aaldyrizkyk/screens/square.dart';
+import 'package:flutter/material.dart';
+
+class Pendonor extends StatelessWidget {
+  final List _posts = [
+    'Nafis Maulidul\nnafismaul@gmail.com\n085712644127\nGolongan Darah B+',
+    'Reza Tokitoki\nreza@gmail.com\n08151238712\nGolongan Darah AB-',
+    'Joko Parjo\njoko@gmail.com\n0851157241\nGolongan Darah O',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    {
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Pendonor',
+          ),
+          backgroundColor: const Color(0xffcd0000),
+        ),
+
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: _posts.length,
+                  itemBuilder: (context, index) {
+                    return MySquare(
+                      child: _posts[index],
+                    );
+                  }),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+}
